863df9b0b177c3f2cbf44734b67dfea62b5e4266
tsekpoe <tsekpoeblessing@gmail.com>
2024-06-15 13:29:11 +0000

    added popup function when 'Open' is clicked
    changed 'search.js' to 'main.js' for all javascript functions
    created 'popup.css' to conatain styles specific to the popup
    added 'id' to 'document_data.js' to track which button was clicked and display corresponding info in the popup

--- static/DocumentStream/assets/js/document_data.js
+++ static/DocumentStream/assets/js/document_data.js
@@ -1,14 +1,13 @@
 const documents = [
-    { title: "Annual Report", description: "Annual report of comapny in the year 2023." },
-    { title: "Employee Handbook", description: "Guidelines and policies for employees of the company." },
-    { title: "Project Plan", description: "Planning document for projects." },
-    { title: "Marketing Strategy", description: "Outline of the marketing strategy for the upcoming year." },
-    { title: "Proposal Letter", description: "Proposal documennt for job." },
-    { title: "IT Security", description: "Policies and procedures for maintaining IT security." },
-    { title: "Training Manual", description: "Comprehensive guide for new employees training." },
-    { title: "Minutes", description: "Minutes from the company-wide meeting held on June 03, 2024." },
-    { title: "Project Launch Plan", description: "Plan for the launch of the new projects." },
+    { id: 1, title: "Annual Report", description: "Annual report of company in the year 2023." },
+    { id: 2, title: "Employee Handbook", description: "Guidelines and policies for employees of the company." },
+    { id: 3, title: "Project Plan", description: "Planning document for projects." },
+    { id: 4, title: "Marketing Strategy", description: "Outline of the marketing strategy for the upcoming year." },
+    { id: 5, title: "Proposal Letter", description: "Proposal documennt for job." },
+    { id: 6, title: "IT Security", description: "Policies and procedures for maintaining IT security." },
+    { id: 7, title: "Training Manual", description: "Comprehensive guide for new employees training." },
+    { id: 8, title: "Minutes", description: "Minutes from the company-wide meeting held on June 03, 2024." },
+    { id: 9, title: "Project Launch Plan", description: "Plan for the launch of the new projects." },
 ];
-
 
 // I didn't know what data to develop with so I used some dummy data...
